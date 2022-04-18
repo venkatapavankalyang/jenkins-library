fdacabeebbfa16a66f1bd0e611f461235e987d09
pavan <venkatapavankalyang@gmail.com>
2022-04-18 19:33:58 +0530

    added

--- vars/hello.groovy
+++
@@ -1,5 +0,0 @@
-def call() {
-    def PIPELINE_PROPS = readProperties file: 'pipelineproperties/pipeline.properties'
-    gitCheckOut("${PIPELINE_PROPS["MAVEN_URL"]}", "$params.Branch")
-    compileSource("${PIPELINE_PROPS["MAVEN_BUILD_COMMAND"]}")
-}
