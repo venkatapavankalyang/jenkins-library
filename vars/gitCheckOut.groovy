6328e27b538beee33ab0982422a7b3b94d0bae0b
pavan <venkatapavankalyang@gmail.com>
2022-04-19 16:20:15 +0530

    added

--- vars/gitCheckOut.groovy
+++ vars/gitCheckOut.groovy
@@ -1,5 +1,5 @@
 
-def call(final string URL, String Branch) {
+def call(final String URL, String Branch) {
     checkout([$class           : 'GitSCM',
               branches         : [[name: "$Branch"]],
               extensions       : [[$class: 'CloneOption', timeout: 60,]],
