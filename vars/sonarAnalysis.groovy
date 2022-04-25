abb610a9e265ae697871599a915bc3e654853f55
pavan <venkatapavankalyang@gmail.com>
2022-04-25 15:57:18 +0530

    added

--- vars/sonarAnalysis.groovy
+++ vars/sonarAnalysis.groovy
@@ -1,9 +1,14 @@
-def call(String sonarProjectKey, String sonarProjectName, String scannerHome, String sonarProjectVersion) {
-    withSonarQubeEnv('sonarqube') {
-        sh "${scannerHome}/bin/sonar-scanner    " + {
-            "-Dsonar.projectKey=${sonarProjectKey} " +
-            "-Dsonar.projectName=\"${sonarProjectName}\" " +
-            "-Dsonar.projectVersion=${sonarProjectVersion}"
-        }
+def call(String ProjectKey,String ProjectName) {
+    def sonarDir = tool name: 'scanner-project', type: 'hudson.plugins.sonar.SonarRunnerInstallation'
+    withSonarQubeEnv(credentialsId: 'comp-sonar') {
+        sh "${sonarDir}/bin/sonar-scanner \
+           -Dsonar.ProjectKey=${ProjectName} \
+           -Dsonar.ProjectName=${ProjectName} \
+           -Dsonar.ws.timeout=60 \
+           -Dsonar.sources=. \
+           -Dsonar.sourceEncoding=UTF-8 \
+           -Dsonar.java.binaries=. \
+           -Dsonar.language=java \
+           -Dsonar.java.source=1.8"
     }
 }
