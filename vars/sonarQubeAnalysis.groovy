03eba5a854cb7e05f3f0c6478874ac4e7c2164a3
pavan <venkatapavankalyang@gmail.com>
2022-04-25 20:49:04 +0530

    added

--- vars/sonarQubeAnalysis.groovy
+++
@@ -1,9 +0,0 @@
-
-sonar.projectKey=$JOB_NAME
- sonar.projectName=$JOB_NAME
- sonar.projectVersion=$BUILD_NUMBER
- sonar.exclusions=vendor/**, storage/**, resources/**
- sonar.language=java
- sonar.sources=$WORKSPACE
- sonar.sourceEncoding=UTF-8
- sonar.java.binaries=$WORKSPACE/build/classes/java/main
