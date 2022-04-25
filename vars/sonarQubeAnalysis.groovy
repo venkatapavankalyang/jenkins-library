5f9d59111104401b33956773d764ef1e530c6dff
pavan <venkatapavankalyang@gmail.com>
2022-04-25 17:49:48 +0530

    added

--- vars/sonarQubeAnalysis.groovy
+++ vars/sonarQubeAnalysis.groovy
@@ -1,5 +1,5 @@
  def call(String projectKey, String projectName) {
-    def scannerhome = tool = tool name: 'scanner-project'
+    def scannerhome = tool  name: 'scanner-project'
     withSonarQubeEnv(credentialsId: 'sonarqube_token') {
         sh "${scannerhome}/bin/sonar-scanner \
            -Dsonar.ProjectKey=${projectKey} \
