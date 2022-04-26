c146ec464eaab44c3c1fe318baa02c4320517527
pavan <venkatapavankalyang@gmail.com>
2022-04-26 17:28:43 +0530

    added

--- vars/sonarQubeAnalysis.groovy
+++ vars/sonarQubeAnalysis.groovy
@@ -1,5 +1,5 @@
 def call(String ProjectKey, String ProjectName, String ProjectVersion, String sonarLogin, String hostUrl) {
-   def sonarqubeScannerHome = tool name: 'sonar-scanner', type: 'hudson.plugins.sonar.SonarRunnerInstallation'
+   def sonarqubeScannerHome = tool name: 'sonarqube-7.9.5', type: 'hudson.plugins.sonar.SonarRunnerInstallation'
     withSonarQubeEnv(credentialsId: '9090') {
             sh "${sonarqubeScannerHome}/bin/sonar-scanner "
                    "-Dsonar.host.url= ${hostUrl}"
