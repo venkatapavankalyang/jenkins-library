40c7e6d446d8d9fa3bfe12d5aca64c537846a960
pavan <venkatapavankalyang@gmail.com>
2022-04-29 00:21:25 +0530

    added .

--- vars/mavenBuild.groovy
+++ vars/mavenBuild.groovy
@@ -1,13 +1,15 @@
-def call() {
-    try {
-        sh '''
-	      sh "mvn package"
-		  withSonarQubeEnv("SonarQube-7.5") {
-                 sh "mvn sonar:sonar"
-          }
-        '''
-    }
-    catch(Exception e) {
-        println(e)
+def call(final String PROJECT_NAME,String MVN_BUILD_COMMAND, String CUSTOMPATH, String WORKSPACE) {
+    def CUSTOMPATH
+    CUSTOMPATH = "${WORKSPACE}${PROJECT_NAME}
+    if (str.contains("Maven_Build")) {
+        dir("${WORKSPACE}/PROJECT_NAME/") {
+            sh """
+		    cd "${WORKSPACE}{PROJECT_NAME}
+			mvn clean packge
+            withSonarQubeEnv("SonarQube-7.5") {
+              sh "mvn sonar:sonar"
+			}
+           """
+        }
     }
 }
