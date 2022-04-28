e77d0b00f65cc30823e6e9a275f5094b79dfa27d
pavan <venkatapavankalyang@gmail.com>
2022-04-28 20:47:41 +0530

    added .

--- vars/mavenBuild.groovy
+++ vars/mavenBuild.groovy
@@ -1,8 +1,13 @@
-def call ()
-try {
-    sh """
-	    mvn package
-		withSonarQubeEnv("SonarQube-7.5") {
+def call() {
+    try {
+        sh """
+	      mvn package
+		  withSonarQubeEnv("SonarQube-7.5") {
                       sh "mvn sonar:sonar"
-        }
-	}
+          }
+          """
+    }
+    catch (Exception e) {
+        println(e)
+    }
+}
